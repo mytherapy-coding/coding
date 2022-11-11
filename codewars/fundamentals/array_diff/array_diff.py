f368437d56e6a59b6d37aeec285ba9e75f18c216
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2022-11-10 22:18:06 -0500

    Create array_diff.py

---
+++ codewars/fundamentals/array_diff/array_diff.py
@@ -0,0 +1,33 @@
+def array_diff1(a: list, b: list) -> list:
+    c = []
+    for x in a:
+        if x not in b:
+            c.append(x)
+
+    return c
+
+def array_diff2(a: list, b: list) -> list:
+    b = set(b)
+    return [x for x in a if x not in b]
+
+def array_diff3(a, b):
+    c = []
+    b = set(b)
+    for x in a:
+        if x not in b:
+            c.append(x)
+    return c
+            
+tab = (
+    ([], [], []),
+    ([], [1, 2], []),
+    ([10, 20], [], [10, 20]),
+    ([3, 6, 7], [7], [3, 6]),
+)
+
+for f in array_diff1, array_diff2, array_diff3:
+    for a, b, c in tab:
+        assert f(a, b) == c
+        print(a,b,c)
+
+
