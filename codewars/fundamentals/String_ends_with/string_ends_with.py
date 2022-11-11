f73fd1eb5e3d1b09a24052b2efdc7f74dda58bc7
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2022-11-11 14:05:22 -0500

    Update string_ends_with.py

--- codewars/fundamentals/String_ends_with/string_ends_with.py
+++ codewars/fundamentals/String_ends_with/string_ends_with.py
@@ -1,2 +1,25 @@
 def solution(string, ending):
     return string.endswith(ending)
+
+
+
+def solution1(a, b):
+    for i in range(len(b)):
+        if b[i] != a[-len(b)+i]:
+            return False
+    return True
+
+
+tab = (
+    ("forever", "ever", True),
+    ("endswith", "end", True),
+    ("love you", "love", True),
+    ("timeshare", "share", [True),
+    ("unrealistic", "realistic", True),
+    ("copay", "pay", True),
+)
+
+for f in solution, solution1:
+    for a, b, c in tab:
+        assert f(a, b) == c, f'failed test on {f.__name__}({a}, {b}), expected {c}'
+
