b424ea374525cf924d3b59fe4698c82f526d3eef
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2022-11-11 11:43:33 -0500

    Update array_diff.py

--- codewars/fundamentals/array_diff/array_diff.py
+++ codewars/fundamentals/array_diff/array_diff.py
@@ -1,4 +1,13 @@
 
+
+def array_diff0(a: list, b: list) -> list:
+    c = a[:]
+    for x in b:
+        for i in range(a.count(x)):
+            c.remove(x)
+
+    return c
+
 def array_diff1(a: list, b: list) -> list:
     c = []
     for x in a:
@@ -7,30 +16,32 @@ def array_diff1(a: list, b: list) -> list:
 
     return c
 
-def array_diff2(a, b):
-    c = []
+
+def array_diff2(a: list, b: list) -> list:
     b = set(b)
+    c = []
     for x in a:
         if x not in b:
             c.append(x)
+
     return c
+
 
 def array_diff3(a: list, b: list) -> list:
     b = set(b)
     return [x for x in a if x not in b]
 
 
+
 tab = (
     ([], [], []),
     ([], [1, 2], []),
+    ([5, 5, 7], [], [5, 5, 7]),
     ([10, 20], [], [10, 20]),
-    ([3, 6, 7], [7], [3, 6]),
+    ([3, 7, 6, 7], [7, 2], [3, 6]),
     ([9, 9, 7], [7], [9, 9]),
 )
 
-for f in array_diff1, array_diff2, array_diff3:
+for f in array_diff0, array_diff1, array_diff2, array_diff3:
     for a, b, c in tab:
-        assert f(a, b) == c
-        print(a,b,c)
-
-
+        assert f(a, b) == c, f'failed test on {f.__name__}({a}, {b}), expected {c}'
