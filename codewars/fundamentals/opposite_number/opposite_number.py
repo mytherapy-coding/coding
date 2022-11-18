70860c42d59a88398173477167594c7402a6b84e
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2022-11-18 14:16:40 -0500

    Create opposite_number.py

---
+++ codewars/fundamentals/opposite_number/opposite_number.py
@@ -0,0 +1,33 @@
+def opposite(number: float) -> float:
+    return -number
+
+def test_opposite():
+    
+    tab: tuple[tuple[float, float], ...] = (
+        (4, -4),
+        (5, -5),
+        (3, -3),
+        (5, -5),
+        (19, -19),
+        (0, 0),
+        (-16, 16),
+        (-4, 4),
+        (-45, 45),
+        (98, -98),
+        (0.87, -0.87),
+        (34, -34),
+        (-76, 76),
+        (-34, 34),
+        (-2398, 2398),
+        (76.9, -76.9),
+        (654, -654),
+    )
+        
+    for number, expected in tab:
+        result = opposite(number)
+        assert result == expected, f'failed test on ({number}, {expected=}, {result=}'
+        
+    print("end of tests")   
+       
+    
+test_opposite()
