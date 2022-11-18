7f083c9cc213526a8b7da5e5b4affc6d90c630c2
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2022-11-18 18:15:14 -0500

    Update reverse_words.py

--- codewars/fundamentals/reverse_words/reverse_words.py
+++ codewars/fundamentals/reverse_words/reverse_words.py
@@ -1,19 +1,21 @@
-def reverse_words1(text):
+def reverse_words1(text: str) -> str:
     words = text.split(" ")
     res = []
     for w in words:
         res.append(w[::-1])
     return " ".join(res)
     
-def reverse_words2(text):
+    
+def reverse_words2((text: str) -> str:
     return " ".join(w[::-1] for w in text.split(" "))
+    
     
 def text_reverse_words():
     tab: tuple[tuple[str, str], ...] = (
         ('hi', 'ih'),
         ('hello', 'olleh'),
         ('Hello world', 'olleH dlrow'),
-    )
+    )        
     for f in reverse_words1, reverse_words2:     
         for text, expected in tab:
             result = f(text)
