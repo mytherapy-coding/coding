30dcacb5312c23a4da90f0322c88d1b6176111d0
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-02-07 15:31:51 -0500

    added accumulate and reduce

---
+++ mypy/myfunctools/reduce.py
@@ -0,0 +1,29 @@
+import operator
+import functools
+import math
+
+
+def reduce(f, iterable, initial=None):
+    it = iter(iterable)
+    if initial is None:
+        res = next(it)
+    else:
+        res = initial
+    for e in it:
+        res = f(res, e)
+    return res
+
+
+values = [1000, 20, 30, 5, 5, 10]
+
+print(reduce(operator.add, values))
+print(sum(values))
+print(reduce(operator.add, values, 0))
+print(reduce(operator.mul, range(1, 6)))
+
+print(reduce(operator.mul, [2]*64))
+print(2**64)
+print(functools.reduce(operator.mul, [2]*64))
+
+print(reduce(operator.mul, values))
+print(math.prod(values))
---
+++ mypy/myitertools/accumulate.py
@@ -0,0 +1,61 @@
+import operator
+import itertools
+
+
+def gen_sum(iterable):
+    it = iter(iterable)
+    res = next(it)
+    yield res
+    for e in it:
+        res = operator.add(res, e)
+        yield res
+
+
+def gen_prod(iterable):
+    it = iter(iterable)
+    res = next(it)
+    yield res
+    for e in it:
+        res = operator.mul(res, e)
+        yield res
+
+
+def gen_min(iterable):
+    it = iter(iterable)
+    res = next(it)
+    yield res
+    for e in it:
+        res = min(res, e)
+        yield res
+
+
+def accumulate(iterable, f=operator.add, *, initial=None):
+    it = iter(iterable)
+    if initial is None:
+        try:
+            res = next(it)
+        except StopIteration:
+            return
+    else:
+        res = initial
+    yield res
+    for e in it:
+        res = f(res, e)
+        yield res
+
+
+values = [10, 20, 30, 5, 5, 10]
+print(list(gen_sum(values)))
+print(list(gen_prod(values)))
+print(list(gen_min(values)))
+
+for acc in accumulate, itertools.accumulate:
+    print(acc)
+    print(list(acc(values, operator.add)))
+    print(list(acc(values, operator.mul)))
+    print(list(acc(values, min)))
+    print(list(acc([], min, )))
+    print(list(acc([], min, initial=0)))
+    print(list(acc(values, operator.add, initial=1000)))
+    print(list(acc(values)))
+
