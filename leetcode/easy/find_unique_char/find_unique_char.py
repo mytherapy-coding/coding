1dcbb6ace2c623b8f8abadb626c792d5afc9e716
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-02-09 20:46:04 -0500

    added leetcode find unique character problem

---
+++ leetcode/easy/find_unique_char/find_unique_char.py
@@ -0,0 +1,18 @@
+import collections
+
+def firstUniqChar0(s):
+    count = collections.Counter(s)
+    print(count)
+    for i, ch in enumerate(s):
+        if count[ch] == 1:
+            return i
+    return -1
+
+def firstUniqChar1(s):
+    count = collections.Counter(s)
+    return next((i for i, ch in enumerate(s) if count[ch] == 1), -1)
+
+
+print(firstUniqChar1("loveleetcode"))
+print(firstUniqChar1("leetcode"))
+print(firstUniqChar1("445669357203525"))
