818bcd7930364b2d9803979490a6375a01244fb4
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-05-30 14:01:59 -0400

    added parentheses.py

--- problems/parentheses.py
+++ problems/parentheses.py
@@ -15,33 +15,67 @@ Invalid:
 ((()
 '''
 
-from collections import deque
-
-from collections import deque
 
 def isbalanced0(expr: str) -> bool:
-    stack = deque()
-    parentheses_map = {
-        ")": "(",
-        "]": "[",
-        "}": "{"
-    }
+    counter = 0
 
     for char in expr:
-        if char in parentheses_map.values():
+        if char == '(':
+            counter += 1
+        elif char == ')':
+            counter -= 1
+
+        if counter < 0:
+            # More closing parentheses encountered without matching opening parentheses
+            return False
+
+    return counter == 0
+
+
+def isbalanced1(expr: str, stack=None) -> bool:
+    if stack is None:
+        stack = []
+    for char in expr:
+        if char == '(':
             stack.append(char)
-        elif char in parentheses_map:
+        elif char == ')':
             if not stack:
                 return False
-            left_parenthes = stack.pop()
-            if left_parenthes != parentheses_map[char]:
+            stack.pop()
+    return not stack
+
+
+def isbalanced2(expr: str, mkstack=list) -> bool:
+    stack = mkstack()
+    for char in expr:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
+
+
+def isbalanced3(expr: str, mkstack=list) -> bool:
+    stack = mkstack()
+    opening_parentheses = ("(", "[", "{")  # "([{"
+    closing_parentheses = (")", "]", "}")
+
+    for char in expr:
+        if char in opening_parentheses:
+            stack.append(char)
+        elif char in closing_parentheses:
+            if not stack:
+                return False
+            opening_paren = stack.pop()
+            if opening_parentheses.index(opening_paren) != closing_parentheses.index(char):
                 return False
 
     return not stack
 
 
-
-def isbalanced01(expr: str, mkstack =deque) -> bool:
+def isbalanced4(expr: str, mkstack=list) -> bool:
     stack = mkstack()
     parentheses_map = {
         "(": ")",
@@ -61,99 +95,32 @@ def isbalanced01(expr: str, mkstack =deque) -> bool:
     return not stack
 
 
-def isbalanced(expr: str) -> bool:
-    stack = deque()
-    opening_parentheses = ["(", "[", "{"]
-    closing_parentheses = [")", "]", "}"]
+def test():
+    from collections import deque
+    class Stack:
+        def __init__(self):
+            self.items = []
 
-    for char in expr:
-        if char in opening_parentheses:
-            stack.append(char)
-        elif char in closing_parentheses:
-            if not stack:
-                return False
-            opening_paren = stack.pop()
-            if opening_parentheses.index(opening_paren) != closing_parentheses.index(char):
-                return False
+        def append(self, x: any):
+            self.items.append(x)
 
-    return len(stack) == 0
+        def pop(self) -> any:
+            return self.items.pop()
 
+        def __bool__(self) -> bool:
+            return bool(self.items)
 
-def isbalanced1(expr: str) -> bool:
-    counter = 0
+    for expr in "()()()", "()(", "()":
+        for isbalanced in isbalanced0, isbalanced1, isbalanced2:
+            print(expr, isbalanced(expr))
+            print()
 
-    for char in expr:
-        if char == '(':
-            counter += 1
-        elif char == ')':
-            counter -= 1
-
-        if counter < 0:  # More closing parentheses encountered without matching opening parentheses
-            return False
-
-    return counter == 0
+    for expr in "[](){}(", "()(", "()":
+        for isbalanced in isbalanced3, isbalanced4:
+            print(expr, isbalanced(expr))
+            for mkstack in deque, list, Stack:
+                print(expr, isbalanced(expr, mkstack=mkstack))
+            print()
 
 
-def isbalanced2(expr: str, stack=None) -> bool:
-    if stack is None:
-        stack = deque()
-    for char in expr:
-        if char == '(':
-            stack.append(char)
-        elif char == ')':
-            if not stack:
-                return False
-            stack.pop()
-    return not stack
-
-
-def isbalanced3(expr: str, stack=None) -> bool:
-    if stack is None:
-        stack = deque()
-    for char in expr:
-        if char == '(':
-            stack.append(char)
-        elif char == ')':
-            if not stack:
-                return False
-            stack.pop()
-    return not stack
-
-
-from collections import deque
-
-
-def isbalanced4(expr: str, mkstack=deque) -> bool:
-    stack = mkstack()
-    for char in expr:
-        if char == '(':
-            stack.append(char)
-        elif char == ')':
-            if not stack:
-                return False
-            stack.pop()
-    return not stack
-
-expr = "[](){}("
-print(isbalanced0(expr))
-
-
-print(isbalanced0("()("))
-print(isbalanced01("()("))
-print(isbalanced1("()"))
-print(isbalanced2("()"))
-expr = "()"
-print(isbalanced2(expr))
-
-print(isbalanced2(expr, []))
-print(isbalanced2(expr, deque()))
-
-print(isbalanced3(expr))
-print(isbalanced3(expr, deque()))
-print(isbalanced3(expr, list()))
-
-print(isbalanced4(expr))
-print(isbalanced4(expr, mkstack=deque))
-print(isbalanced4(expr, mkstack=list))
-
-
+test()
