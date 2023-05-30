aa061b6290026ab8e909aa0d363329e447c467f2
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-05-30 13:09:19 -0400

    added count_char_in_string

---
+++ codewars/fundamentals/count_char_in_string/count_char_in_string.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from collections import Counter
+
+
+def count(s: str) -> dict[str, int]:
+    res: dict[str, int] = {}
+    for ch in s:
+        res[ch] = res.get(ch, 0) + 1
+    return res
+
+
+def count1(s: str) -> defaultdict[str, int]:
+    res = defaultdict(int)
+    for ch in s:
+        res[ch] += 1
+    return res
+
+
+def count2(s: str) -> Counter[str]:
+    return Counter(s)
+
+
+print(count("HeLlo"))
+print(count("HeLlo'"))
+print(count(""))
+
+print(count1("HeLlo"))
+print(count1("HeLlo'"))
+print(count1(""))
+
+print(count2("HeLlo"))
+print(count2("HeLlo'"))
+print(count2(""))
