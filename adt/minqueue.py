e63a8dffbc636fc3ec95485447560f995e375c80
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-06-05 22:30:30 -0400

    added minqueue

---
+++ adt/minqueue.py
@@ -0,0 +1,111 @@
+class MinQeue0:
+    def __init__(self, seq=()):
+        self.items = []
+        self.extend(seq)
+
+    def extend(self, iterable):
+        self.items.extend(iterable)
+
+    def add(self, x):
+        self.items.append(x)
+
+    def get_min(self):
+        return min(self.items)
+
+    def remove_min(self):
+        min_item = self.get_min()
+        self.items.remove(min_item)
+        return min_item
+
+    def __repr__(self):
+        return f'{type(self).__qualname__}({self.items})'
+
+    def __len__(self):
+        return len(self.items)
+
+    def __bool__(self):
+        return bool(self.items)
+
+
+class MinQeue1:
+    def __init__(self, seq=()):
+        self.items = []
+        self.min_item = None
+        self.extend(seq)
+
+    def extend(self, iterable):
+        self.items.extend(iterable)
+        self.min_item = min(self.items, default=None)
+
+    def add(self, x):
+        self.items.append(x)
+        if self.min_item is None or x < self.min_item:
+            self.min_item = x
+
+    def get_min(self):
+        if not self.items:
+            raise ValueError("the container is empty")
+        return self.min_item
+
+    def remove_min(self):
+        min_item = self.get_min()
+        self.items.remove(min_item)
+        self.min_item = min(self.items, default=None)
+        return min_item
+
+    def __repr__(self):
+        return f'{type(self).__qualname__}({self.items})'
+
+    def __len__(self):
+        return len(self.items)
+
+    def __bool__(self):
+        return bool(self.items)
+
+
+class MinQeue2:
+    def __init__(self, seq=()):
+        self.items = []
+        self.extend(seq)
+
+    def extend(self, iterable):
+        self.items.extend(iterable)
+        self.items.sort(reverse=True)
+
+    def add(self, x):
+        self.items.append(x)
+        self.items.sort(reverse=True)
+
+    def get_min(self):
+        return self.items[-1]
+
+    def remove_min(self):
+        return self.items.pop()
+
+    def __repr__(self):
+        return f'{type(self).__qualname__}({self.items})'
+
+    def __len__(self):
+        return len(self.items)
+
+    def __bool__(self):
+        return bool(self.items)
+
+
+def test():
+    for min_queue in MinQeue0, MinQeue1, MinQeue2:
+        que = min_queue([20, 10, 30, 20])
+        print('queue: ', que)
+        for _ in range(2):
+            print('get:   ', que.get_min())
+            print('remove:', que.remove_min())
+        que.add(5)
+        print('queue: ', que)
+        print('get:   ', que.get_min())
+        for _ in range(3):
+            print('remove:', que.remove_min())
+
+        print()
+
+
+test()
