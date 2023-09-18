cd5a692c5431cf8eab5377b353c6a67c5ce95571
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-09-17 20:18:49 -0400

    leetcode/leetcode_75/determine_if_two_strings_are_close

---
+++ leetcode/leetcode_75/determine_if_two_strings_are_close/determine_if_two_strings_are_close.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+def closeStrings(word1: str, word2: str) -> bool:
+    if len(word1) != len(word2):
+        return False
+    counter1 = Counter(word1)
+    counter2 = Counter(word2)
+    return counter1.keys() == counter2.keys() and sorted(counter1.values()) == sorted(counter2.values())
+
+
+
+word1 = "abc"
+word2 = "bca"
+print(closeStrings(word1, word2))
+word1 = "a"
+word2 = "aa"
+print((closeStrings(word1, word2)))
+
