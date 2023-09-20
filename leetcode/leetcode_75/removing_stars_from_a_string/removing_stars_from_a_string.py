672dcacf37a2d5216d4ecbd38fc34d75ced6b67e
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-09-20 17:56:09 -0400

    leetcode/leetcode_75/removing_stars_from_a_string/

---
+++ leetcode/leetcode_75/removing_stars_from_a_string/removing_stars_from_a_string.py
@@ -0,0 +1,24 @@
+def removeStars1(s: str) -> str:
+    new_s = list(s)
+    i = 1
+    while i < len(new_s):
+        if new_s[i] == "*":
+            del new_s[i-1:i+1]
+            i -= 1
+        else:
+            i += 1
+    return ''.join(new_s)
+
+
+def removeStars2(s: str) -> str:
+    res = []
+    for ch in s:
+        if ch != "*":
+            res.append(ch)
+        else:
+            res.pop()
+    return ''.join(res)
+
+
+s = "leet**cod*e"
+print(removeStars2(s))
