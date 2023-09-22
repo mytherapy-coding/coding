8b6b1c8c37fd47955888653c17fbaca9c911fd9c
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-09-21 21:35:43 -0400

    leetcode/leetcode_75/number_of_recent_calls/

---
+++ leetcode/leetcode_75/number_of_recent_calls/number_of_recent_calls.py
@@ -0,0 +1,29 @@
+class RecentCounter0:
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        return sum(t - 3000 <= r <= t for r in self.requests)
+
+
+class RecentCounter:
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        res = [t - 3000 <= r for r in self.requests]
+        return len(res)
+
+
+recentCounter = RecentCounter()
+print(recentCounter.requests)
+n = recentCounter.ping(1)  # requests = [1], range is [-2999,1], return 1
+print(recentCounter.requests, n)
+m = recentCounter.ping(100)  # requests = [1, 100], range is [-2900,100], return 2
+print(recentCounter.requests, m)
+q = recentCounter.ping(3001)  # requests = [1, 100, 3001], range is [1,3001], return 3
+print(recentCounter.requests, q)
+s = recentCounter.ping(3002)  # requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+print(recentCounter.requests, s)
