f8b17b690c559a24f605fe8394f3c9fcd5672e10
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-09-30 18:37:04 -0400

    leetcode/leetcode_75/count_good_nodes_in_binary_tree/

---
+++ leetcode/leetcode_75/count_good_nodes_in_binary_tree/count_good_nodes_in_binary_tree.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def count_good(root: TreeNode | None, max_val: int = float('-inf')) -> int:
+    return count_good(root.left, max(root.val, max_val)) + count_good(root.right, max(root.val, max_val)) + (root.val >= max_val) if root else 0
+
+
+root = TreeNode(10, TreeNode(20, TreeNode(30)), TreeNode(5, None, TreeNode(15)))
+print(count_good(root))
