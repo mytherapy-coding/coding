5fba1269cea48640ad50b8574c705e9b5bc2194a
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-10-20 14:16:46 -0400

    leetcode/leetcode_150/longest_common_prefix/

---
+++ leetcode/leetcode_150/longest_common_prefix/longest_common_prefix.py
@@ -0,0 +1,14 @@
+def longestCommonPrefix(strs: list[str]) -> str:
+    for k, t in enumerate(zip(*strs)):
+        t_set = set(t)
+        print(t_set)
+        if len(t_set) != 1:
+            return strs[0][:k]
+    return min(strs, key= lambda w: len(w))
+
+
+strs = ["flower","flow","flight"]
+print(longestCommonPrefix(strs))
+
+strs = ["dog","racecar","car"]
+print(longestCommonPrefix(strs))
