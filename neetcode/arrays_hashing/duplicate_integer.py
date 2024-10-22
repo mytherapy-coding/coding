9fe0f78e809450f526157d614705d30dc837719e
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2024-10-22 18:12:55 -0400

    neetcode/arrays_hashing/duplicate_integer.py

--- neetcode/arrays_hashing/duplicate_integer.py
+++ neetcode/arrays_hashing/duplicate_integer.py
@@ -1,6 +1,12 @@
 from collections import Counter
+
 def hasDuplicate(nums: list[int]) -> bool:
-        c = Counter(nums)
-        return any(n > 1 for n in c.values())
+    
+        d = Counter()
+        for v in nums:
+                d[v] += 1
+                if d[v] == 2:
+                        return True
+        
 
 print(hasDuplicate([1,2,2,5,7,5]))
