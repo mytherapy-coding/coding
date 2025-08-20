86a4f235979c87b54aaa7d4cc138a100d8734946
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-08-20 18:32:57 -0400

    climbing_ctairs

---
+++ leetcode/easy/climbing_stairs/climbing_stairs.py
@@ -0,0 +1,65 @@
+'''
+if n == 0 => 1
+if n == 1 => 1
+if n == 2 => 2
+- 1+1
+- 2
+if n == 3 => 3
+- 1+1+1
+- 1+2
+- 2+1
+if n == 4 => 5
+- 1+1+1+1
+- 1+1+2
+- 1+2+1
+- 2+1+1
+- 2+2
+'''
+'''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def fib(n: int) -> int:
+            if n in (0, 1):
+                return 1
+            return fib(n-1) + fib(n-2)
+
+        return fib(n)
+
+'''
+'''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        @cache
+        def fib(n: int) -> int:
+            if n in (0, 1):
+                return 1
+            return fib(n-1) + fib(n-2)
+
+        return fib(n)
+'''
+'''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        d = {0:1, 1:1}
+        def fib(n: int) -> int:
+            if n in d:
+                return d[n]
+            d[n] = fib(n-1) + fib(n-2)
+            return d[n]
+            
+        return fib(n)
+'''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        table = [1]*(n+1)
+        for i in range(2, n+1):
+            table[i] = table[i-1] + table[i-2]
+        return table[n]
+
+
+            
+        
+        
+        
+            
+        
