9c24e782556b54b41a17411e52325eb65c815dc1
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-09-17 13:56:00 -0400

    serach insert position

---
+++ leetcode/easy/search_insert_position/search_insert_position.py
@@ -0,0 +1,4 @@
+from bisect import bisect_left
+
+def searchInsert(self, nums: list[int], target: int) -> int:
+    return bisect_left(nums, target)
--- neetcode/unique.py
+++ neetcode/unique.py
@@ -2,16 +2,36 @@ import bisect
 
 
 def unique_numbers(array):
-    unique = []
+    if not array:
+        return []
     x = array[0]
-    unique.append(x)
+    unique = [x]
     while True:
         ind = bisect.bisect_left(array, x + 1)
         if ind >= len(array):
             break
-        unique.append(array[ind])
         x = array[ind]
+        unique.append(x)
     return unique
 
 
 print(unique_numbers([7, 7, 7, 7, 8, 9, 10, 10, 11, 12]))
+
+
+
+
+def unique_numbers1(array):
+    if not array:
+        return []
+    x = array[0]
+    unique = [x]
+    while True:
+        ind = bisect.bisect_left(array, x + 1)
+        if ind >= len(array):
+            break
+        x = array[ind]
+        unique.append(x)
+    return unique
+
+
+print(unique_numbers1([7, 7, 7, 7, 8, 9, 10, 10, 11, 12]))
