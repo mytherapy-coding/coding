12d7305b9c12c0f052288e46b05b5f34afd56e28
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-09-18 19:14:52 -0400

    leetcode/easy/binary_search/

---
+++ leetcode/easy/binary_search/sqrt.py
@@ -0,0 +1,17 @@
+def mySqrt(x: int) -> int:
+    left = 0
+    right = x + 1
+    while left + 1 < right:
+        mid = (left + right) // 2
+        if mid * mid == x:
+            return mid
+        if mid * mid > x:
+            right = mid
+        else:
+            left = mid 
+    return left
+
+
+            
+print(mySqrt(1))
+        
