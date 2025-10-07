e27e50304e6fc653c8adb7a4510defe86b1362af
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-10-07 14:42:37 -0400

    kth missing number

---
+++ leetcode/easy/kth_missing_positive_number.py
@@ -0,0 +1,14 @@
+def findKthPositive(arr: list[int], k: int) -> int:
+    # arr = [2,3,4,7,11], k = 5
+    # [1,5,6,8,9,10,12,13,...]
+    count = 0
+    for x in range(1, k + len(arr) + 1):
+        if x not in arr:
+            count += 1
+            if count == k:
+                return x
+
+arr = [2,3,4,7,11]
+k = 5
+kth_number = findKthPositive(arr, k)
+print(kth_number)
