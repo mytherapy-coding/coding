241d26de778be5953fe60f33d291d69196b44fea
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-10-20 01:26:57 -0400

    arrangcoins

--- leetcode/easy/binary_search/arrangcoins.py
+++ leetcode/easy/binary_search/arrangcoins.py
@@ -1,3 +1,47 @@
-def arrangeCoins(n: int) -> int:
+import math
 
+def arrangeCoins0(n: int) -> int:
+    coins = 0
+    for k in range(1, n + 2):
+        coins += k
+        if coins > n:
+            return k - 1
+        
+
+def arrangeCoins1(n: int) -> int:
+    for k in range(1, n + 2):
+        coins = k*(k + 1)//2
+        if coins > n:
+            return k - 1
+        
+def arrangeCoins2(n: int) -> int:
+    low = 1
+    high = n + 1
+    while low < high:
+        mid = (low + high)//2 
+        coins = mid*(1 + mid)//2 
+        if coins == n:
+            return mid
+        if coins < n:
+            low = mid  + 1
+        else:
+            high = mid 
+    return low - 1
     
+def arrangeCoins(n: int) -> int:
+    return int((-1 + math.sqrt(1 + 8 * n)) / 2)
+
+
+
+#### ####
+# ## ### ##
+
+# k(1 + k)/2 <= n < (k+1)(k+2)/2
+n = 11
+print(arrangeCoins(n))
+
+#
+##
+###
+####
+#
--- problems/minstack.py
+++ problems/minstack.py
@@ -5,12 +5,19 @@ write min stack that supports push, pop, top, empty and get min
 class MinStack:
     def __init__(self):
         self.values = []
+        self.minval = []
 
     def push(self, x):
         self.values.append(x)
+        if self.minval:
+            self.minval.append(min(self.minval[-1], x))
+        else:
+            self.minval.append(x)
 
     def pop(self):
+        self.minval.pop()
         return self.values.pop()
+    
     
     def top(self):
         return self.values[-1]
@@ -20,6 +27,10 @@ class MinStack:
     
     def __bool__(self):
         return bool(self.values)
+    
+    def getmin(self):
+        return self.minval[-1]
+        
 
   
 
@@ -29,11 +40,14 @@ print(stack.values)
         
 stack.push(10)
 stack.push(20)
+stack.push(5)
 print(stack.values)
 print(stack.top())
+print(stack.getmin(), "getmin")
 x = stack.pop()
 print(stack.values)
 print(x)
+print(stack.getmin(), "getmin")
 print(stack.top())
 print(stack.empty())
 print(stack.pop())
@@ -43,3 +57,4 @@ if stack:
     print("Stack is not empty")
 else:
     print("Stack is empty")
+
