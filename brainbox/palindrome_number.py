df97e828ead676c0f7eaae9f117fbfd2fa7ab84b
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2026-02-03 22:29:07 -0500

    palindrome tests added

--- brainbox/palindrome_number.py
+++ brainbox/palindrome_number.py
@@ -2,3 +2,14 @@ def isPalindrome(x):
     if x < 0:
         return False
     return str(x) == str(x)[::-1]
+
+def test_is_palindrome_true():
+    assert isPalindrome(121)
+    assert isPalindrome(0)
+    assert isPalindrome(1221)
+
+
+def test_is_palindrome_false():
+    assert not isPalindrome(123)
+    assert not isPalindrome(10)
+    assert not isPalindrome(-1)
