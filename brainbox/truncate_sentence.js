2216a9a2e4282573224b6bfcb4bfc8ce0045caff
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2026-02-08 17:55:27 -0500

    truncate_sentence.js

---
+++ brainbox/truncate_sentence.js
@@ -0,0 +1,13 @@
+function truncateSentence(s, k) {
+  return s.split(" ").slice(0, k).join(" ");
+}
+
+function test_truncateSentence() {
+  console.log(truncateSentence("Hello world", 1), "== Hello");
+  console.log(truncateSentence("Hello world", 2), "== Hello world");
+  console.log(truncateSentence("What is your name", 3), "== What is your");
+  console.log(truncateSentence("This is a test sentence", 4), "== This is a test");
+  console.log(truncateSentence("Single", 1), "== Single");
+}
+
+test_truncateSentence();
