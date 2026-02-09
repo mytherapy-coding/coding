826a93f333d1078ff6e8e944205f003d0429ace8
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2026-02-08 20:25:55 -0500

    find_center_star_graph.js

---
+++ brainbox/find_center_star_graph.js
@@ -0,0 +1,19 @@
+function findCenter(edges) {
+  const [a, b] = edges[0];
+  const [c, d] = edges[1];
+
+  if (a === c || a === d) return a;
+  return b;
+}
+
+
+function test_findCenter() {
+  console.log(findCenter([[1, 2], [2, 3], [4, 2]]), "== 2");
+  console.log(findCenter([[1, 3], [2, 3], [3, 4]]), "== 3");
+  console.log(findCenter([[5, 1], [5, 2], [5, 3], [5, 4]]), "== 5");
+  console.log(findCenter([[10, 7], [7, 2]]), "== 7");
+  console.log(findCenter([[100, 1], [50, 100], [100, 3]]), "== 100");
+  console.log(findCenter([[8, 9], [9, 1], [2, 9]]), "== 9");
+}
+
+test_findCenter();
