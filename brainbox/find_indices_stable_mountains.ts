29361db06e0bd0bc5a4671517b76a0a4aac7c027
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2026-02-08 21:20:18 -0500

    find_indices_stable_mountains.ts

---
+++ brainbox/find_indices_stable_mountains.ts
@@ -0,0 +1,26 @@
+export function findStableMountains(height: number[], threshold: number): number[] {
+  const result: number[] = [];
+
+  for (let i = 1; i < height.length; i++) {
+    if (height[i - 1] > threshold) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+
+import { findStableMountains } from "./findStableMountains";
+
+function test_findStableMountains() {
+  console.log(findStableMountains([1,2,3,4,5], 2), "== [3, 4]");
+  console.log(findStableMountains([10,1,10,1,10], 3), "== [1, 3]");
+  console.log(findStableMountains([10,1,10,1,10], 10), "== []");
+  console.log(findStableMountains([5,5,5,5], 4), "== [1, 2, 3]");
+  console.log(findStableMountains([5,5,5,5], 5), "== []");
+  console.log(findStableMountains([2,100], 50), "== [1]");
+  console.log(findStableMountains([1,50,1,50,1], 10), "== [1, 3]");
+}
+
+test_findStableMountains();
